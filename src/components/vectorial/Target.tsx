b4b057b23020cc4ec30eec4e3c5f04354728a625
Alberto Pravisano <alberto.pravisano@beantech.it>
2023-02-13 14:54:14 +0100

    aptic feedback for mobile when score is set

--- src/components/vectorial/Target.tsx
+++ src/components/vectorial/Target.tsx
@@ -9,6 +9,12 @@ interface Props {
 
 const Target: React.FC<Props> = ({ setScore, onRevert }) => {
   const { t } = useTranslation("common");
+
+  const onHandleClick = (score: number) => {
+    window.navigator?.vibrate(200);
+    setScore(score);
+  };
+
   return (
     <Segment basic compact textAlign="center" style={{ margin: "0 auto" }}>
       <svg
@@ -20,7 +26,7 @@ const Target: React.FC<Props> = ({ setScore, onRevert }) => {
         <g>
           <path
             d="M 105,4.9998 C 160.2187,4.9998 204.9998,49.7808 204.9998,104.9998 C 204.9998,160.2185 160.2187,204.9996 105,204.9996 C 49.7808,204.9996 5,160.2185 5,104.9998 C 5,49.7806 49.781,4.9998 105,4.9998 L 105,4.9998 z"
-            onClick={() => setScore(2)}
+            onClick={() => onHandleClick(2)}
             style={{
               fill: "#00ffff",
               stroke: "#000000",
@@ -29,7 +35,7 @@ const Target: React.FC<Props> = ({ setScore, onRevert }) => {
           />
           <path
             d="M 105,44.9999 C 138.1312,44.9999 164.9999,71.8685 164.9999,105 C 164.9999,138.1312 138.1312,164.9999 105,164.9999 C 71.8685,164.9999 44.9999,138.1312 44.9999,105 C 44.9999,71.8685 71.8685,44.9999 105,44.9999 z"
-            onClick={() => setScore(1)}
+            onClick={() => onHandleClick(1)}
             style={{
               fill: "red",
               stroke: "#000000",
@@ -38,7 +44,7 @@ const Target: React.FC<Props> = ({ setScore, onRevert }) => {
           />
           <path
             d="M 105,85 C 116.0437,85 125,93.9562 125,105 C 125,116.0437 116.0437,125 105,125 C 93.9562,125 85,116.0437 85,105 C 85,93.9562 93.9562,85 105,85 z"
-            onClick={() => setScore(0)}
+            onClick={() => onHandleClick(0)}
             style={{
               fill: "yellow",
               stroke: "#000000",
@@ -47,7 +53,7 @@ const Target: React.FC<Props> = ({ setScore, onRevert }) => {
           />
           <path
             d="M 105,100 C 107.7609,100 110,102.239 110,105 C 110,107.7609 107.7609,110 105,110 C 102.239,110 100,107.7609 100,105 C 100,102.239 102.239,100 105,100 z"
-            onClick={() => setScore(0)}
+            onClick={() => onHandleClick(0)}
             style={{
               fill: "yellow",
               stroke: "#000000",
@@ -66,7 +72,7 @@ const Target: React.FC<Props> = ({ setScore, onRevert }) => {
           {t("buttons.revert")} <Icon name="reply" />
         </Button>
       }
-      <Button onClick={() => setScore(-1)}>
+      <Button onClick={() => onHandleClick(-1)}>
         {t("buttons.miss").toUpperCase()}
       </Button>
     </Segment>
